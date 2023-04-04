2c0afc372a8944d4d6a78270cef63aaf4a1375fa
Vladas Zakrevskis <146100@gmail.com>
2023-04-04 23:12:25 +0300

    commod makefile

---
+++ common.mk
@@ -0,0 +1,33 @@
+
+all: desktop
+
+everything: order desktop ios
+
+desktop:
+	./build/build.sh
+
+ios:
+	./build/build.sh ios
+
+lipo:
+	cargo lipo
+
+android:
+	./build/build.sh android
+
+clippy:
+	cargo clippy -- -D clippy::pedantic -A clippy::module-name-repetitions -A clippy::must_use_candidate -A clippy::implicit-hasher -A clippy::missing_errors_doc -A clippy::semicolon_if_nothing_returned -A clippy::return_self_not_must_use -A clippy::default_trait_access -A clippy::needless_pass_by_value -A clippy::missing_panics_doc -A clippy::mismatched_target_os -A clippy::explicit-deref-methods -A clippy::cast-precision-loss -A clippy::module_inception
+
+order:
+	taplo fmt; \
+	cargo +nightly fmt --all; \
+	cargo test --all; \
+	cargo build --all; \
+	make clippy; \
+	typos; \
+	cargo machete
+
+deps:
+	cargo install cargo-machete;\
+    cargo install typos-cli;\
+    cargo install taplo-cli
