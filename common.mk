ff8cbe5b3ab03ebe84c5c2dad4c03ce1b8aca1b2
Vladas Zakrevskis <146100@gmail.com>
2023-10-27 00:00:45 +0300

    test with release

--- common.mk
+++ common.mk
@@ -21,6 +21,8 @@ android:
 clippy:
 	cargo clippy -- -D clippy::pedantic -A clippy::module-name-repetitions -A clippy::must_use_candidate -A clippy::implicit-hasher -A clippy::missing_errors_doc -A clippy::semicolon_if_nothing_returned -A clippy::return_self_not_must_use -A clippy::default_trait_access -A clippy::needless_pass_by_value -A clippy::missing_panics_doc -A clippy::mismatched_target_os -A clippy::explicit-deref-methods -A clippy::cast-precision-loss -A clippy::module_inception
 
+lint: clippy
+
 order:
 	taplo fmt; \
 	cargo +nightly fmt --all; \
@@ -31,7 +33,7 @@ order:
 	    #	cargo machete; \
 
 test:
-	cargo test --all
+	cargo test --all && cargo test --all --release
 
 deps:
 	cargo install cargo-machete;\
