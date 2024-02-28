c914e0e56d6d55c25e326a6eda17804ac2a0b364
Vladas <146100@gmail.com>
2024-02-28 22:34:11 +0200

    allow too many arguments

--- common.mk
+++ common.mk
@@ -19,7 +19,7 @@ android:
 	./build/build.sh android
 
 clippy:
-	cargo clippy -- -D clippy::pedantic -A clippy::no-effect-underscore-binding -A clippy::unnecessary_box_returns -A clippy::module-name-repetitions -A clippy::must_use_candidate -A clippy::implicit-hasher -A clippy::missing_errors_doc -A clippy::semicolon_if_nothing_returned -A clippy::return_self_not_must_use -A clippy::default_trait_access -A clippy::needless_pass_by_value -A clippy::missing_panics_doc -A clippy::mismatched_target_os -A clippy::explicit-deref-methods -A clippy::cast-precision-loss -A clippy::module_inception
+	cargo clippy -- -D clippy::pedantic -A clippy::too_many_arguments -A clippy::no-effect-underscore-binding -A clippy::unnecessary_box_returns -A clippy::module-name-repetitions -A clippy::must_use_candidate -A clippy::implicit-hasher -A clippy::missing_errors_doc -A clippy::semicolon_if_nothing_returned -A clippy::return_self_not_must_use -A clippy::default_trait_access -A clippy::needless_pass_by_value -A clippy::missing_panics_doc -A clippy::mismatched_target_os -A clippy::explicit-deref-methods -A clippy::cast-precision-loss -A clippy::module_inception
 
 lint: clippy
 
