de031f99e85e4416908b23602d8a01dc35b7f5e5
Vladas Zakrevskis <146100@gmail.com>
2024-05-25 19:28:58 +0300

    different profiles for profiling

--- profile.sh
+++ profile.sh
@@ -3,9 +3,14 @@ set -eox pipefail
 source env.sh
 
 echo "APP_NAME: $APP_NAME"
+echo "CARGO_PROFILE_FOR_PROFILING: $CARGO_PROFILE_FOR_PROFILING"
 
 cargo install --locked samply
 
-cargo build -p "$APP_NAME" --profile=release-debug
+cargo build -p "$APP_NAME" --profile="$CARGO_PROFILE_FOR_PROFILING"
 
-samply record ./target/release-debug/"$APP_NAME"
+if [ "$CARGO_PROFILE_FOR_PROFILING" = "dev" ]; then
+    CARGO_PROFILE_FOR_PROFILING="debug"
+fi
+
+samply record ./target/"$CARGO_PROFILE_FOR_PROFILING"/"$APP_NAME"
