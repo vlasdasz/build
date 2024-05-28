ed4eb9f76a39b2979355a129df5f7818fabe307c
Vladas Zakrevskis <146100@gmail.com>
2024-05-29 02:44:51 +0300

    makefile cleanup

--- common.mk
+++ common.mk
@@ -18,16 +18,28 @@ lipo:
 android:
 	./build/build.sh android
 
-clippy:
-	cargo clippy -- -D clippy::pedantic -A clippy::too_many_arguments -A clippy::no-effect-underscore-binding -A clippy::unnecessary_box_returns -A clippy::module-name-repetitions -A clippy::must_use_candidate -A clippy::implicit-hasher -A clippy::missing_errors_doc -A clippy::semicolon_if_nothing_returned -A clippy::return_self_not_must_use -A clippy::default_trait_access -A clippy::needless_pass_by_value -A clippy::missing_panics_doc -A clippy::mismatched_target_os -A clippy::explicit-deref-methods -A unexpected_cfgs -A clippy::module_inception
-
-lint: clippy
+lint:
+	cargo clippy -- \
+	-D clippy::pedantic \
+	-A clippy::too_many_arguments \
+	-A clippy::no-effect-underscore-binding \
+	-A clippy::unnecessary_box_returns \
+	-A clippy::module-name-repetitions \
+	-A clippy::must_use_candidate \
+	-A clippy::missing_errors_doc \
+	-A clippy::return_self_not_must_use \
+	-A clippy::needless_pass_by_value \
+	-A clippy::missing_panics_doc \
+	-A clippy::mismatched_target_os \
+	-A clippy::explicit-deref-methods \
+	-A unexpected_cfgs \
+	-A clippy::module_inception
 
 test:
-	cargo test --all  && \
-    echo debug test: OK && \
-    cargo test --all --release && \
-    echo release test: OK
+	cargo test --all
+	echo debug test: OK
+	cargo test --all --release
+	echo release test: OK
 
 deps:
 	cargo install cargo-machete;\
