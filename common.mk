051b445dc4e9bfef0b25da68b2cca7687dc62395
Vladas Zakrevskis <146100@gmail.com>
2024-07-05 02:01:08 +0300

    remove lint from common makefile

--- common.mk
+++ common.mk
@@ -16,22 +16,6 @@ lipo:
 android:
 	./build/build.sh android
 
-lint:
-	cargo clippy -- \
-	-D clippy::pedantic \
-	-A clippy::too_many_arguments \
-	-A clippy::no-effect-underscore-binding \
-	-A clippy::unnecessary_box_returns \
-	-A clippy::module-name-repetitions \
-	-A clippy::must_use_candidate \
-	-A clippy::missing_errors_doc \
-	-A clippy::return_self_not_must_use \
-	-A clippy::needless_pass_by_value \
-	-A clippy::missing_panics_doc \
-	-A clippy::explicit-deref-methods \
-	-A unexpected_cfgs \
-	-A clippy::module_inception
-
 test:
 	cargo test --all
 	echo debug test: OK
