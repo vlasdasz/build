641d29d9c8388810fe6947bfd0e6451c543e9803
"Vladas <"146100@gmail.com">
2025-02-15 22:13:27 +0200

    flu logs

--- scripts/flight.sh
+++ scripts/flight.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -eox pipefail
+
 source env.sh
 
 EXPORT_OPTIONS_PLIST="export.plist"
@@ -19,11 +20,19 @@ echo "IPA_PATH: $IPA_PATH"
 # Clean build folder
 xcodebuild clean -project "$PROJECT_NAME.xcodeproj" -scheme "$PROJECT_NAME" -configuration Release
 
+echo clean: OK
+
 # Build project
 xcodebuild -project "$PROJECT_NAME".xcodeproj -scheme "$PROJECT_NAME" \
   -sdk iphoneos -configuration Release archive -archivePath "$ARCHIVE_PATH"
 
+echo build: OK
+
 # Export IPA
 xcodebuild -exportArchive -archivePath "$ARCHIVE_PATH" -exportOptionsPlist "$EXPORT_OPTIONS_PLIST" -exportPath "build"
 
+echo export: OK
+
 xcrun altool --upload-app -f "$IPA_PATH" -u 146100@gmail.com -p "$FLIGHT_PASS" --type ios
+
+echo upload: OK
