68424ac980ade9cd6d2308efe08f2e417b86b51b
Vladas Zakrevskis <146100@gmail.com>
2026-01-28 15:33:32 +0200

    build linux to separate folder

--- build-linux.py
+++ build-linux.py
@@ -5,11 +5,13 @@ import subprocess
 
 def run(cmd, check=True, capture_output=False):
     print(f"Running: {cmd}")
-    result = subprocess.run(cmd, check=check, shell=True, capture_output=capture_output, text=True)
+    result = subprocess.run(
+        cmd, check=check, shell=True, capture_output=capture_output, text=True
+    )
     if capture_output:
         return result.stdout.strip()
     return None
 
 
 run("cargo install cross --git https://github.com/cross-rs/cross")
-run(f"cross build --all --release")
+run("cross build --all --release --target x86_64-unknown-linux-gnu")
