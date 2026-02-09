bf7bda433c1c06db7847a3e686fe688514d5c01d
Vladas Zakrevskis <146100@gmail.com>
2026-02-09 16:11:45 +0200

    fixed ios and makefile cleanup

--- common.mk
+++ common.mk
@@ -1,15 +1,6 @@
 
-clean:
-	cargo clean
-
-desktop:
-	./build/build.sh
-
 ios:
-	./build/build.sh ios
-
-lipo:
-	cargo lipo
+	CFLAGS="" SDKROOT="" ./build/build.sh ios
 
 android:
 	./build/build.sh android
@@ -20,34 +11,14 @@ test:
 	cargo test --all --release
 	echo release test: OK
 
-deps:
-	cargo install cargo-machete;\
-    cargo install typos-cli;\
-    cargo install taplo-cli
-
 fly:
 	./build/scripts/flight.sh
 
 profile:
 	./build/scripts/profile.sh
 
-release:
-	cargo build --release
-
-run-release:
-	cargo run --release
-
-plus:
-	cargo build --profile=release-plus
-
-run-plus:
-	cargo run --profile=release-plus
-
 pr:
 	gh pr create --fill
-
-r:
-	cargo run --release
 
 fmt:
 	cargo +nightly fmt --all
@@ -55,5 +26,3 @@ fmt:
 updates:
 	cargo install cargo-upgrades --locked
 	cargo upgrades
-
-.PHONY: test clippy
